f7db1b07773cef20ef3d790b3d5fe001f4cf9b62
Chun Liu <chunliu@users.noreply.github.com>
2018-01-15 15:44:23 +0800

    add script to create resource group

---
+++ azscripts/creatergs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rgname="apimlabrg"
+location="westus"
+
+uName="user"
+domain="@apimlabs.onmicrosoft.com"
+
+i=1
+while [ i -le 90 ]
+do
+    if [ i -lt 10 ]
+    then
+        uName="user0${i}"
+        rgname="apimlabrg0${i}"
+    else
+        uName="user${i}"
+        rgname="apimlabrg${i}"
+    fi
+
+    # Create resource group
+    az group create --location "${location}" --name "${rgname}" >/dev/null
+    if [ "$?" -ne 0 ] 
+    then 
+        echo "az group create failed"
+        exit 1
+    fi
+
+    # Assign contributor role to the corresponding user account
+    az role assignment create --role "Contributor" --assignee "${uName}${domain}" --resource-group "${rgname}" >/dev/null
+    if [ "$?" -ne 0 ] 
+    then 
+        echo "az role assignment failed"
+        exit 1
+    fi
+
+    echo "${rgname} is created and ${uName}${domain} is assigned to it."
+done
+
+echo "All done!"
